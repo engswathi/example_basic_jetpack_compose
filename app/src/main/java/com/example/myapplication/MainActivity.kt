2bd6a9e2a58a7eec049776fbd887cc94b323e1bc
Swathi P G <swathi7181@gmail.com>
2023-10-27 22:56:57 +0530

    Basic Jetpack

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,12 +5,17 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -34,48 +39,63 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
-               var name by remember {
-                   mutableStateOf("")
-               }
-                
+                var name by remember {
+                    mutableStateOf("")
+                }
+
                 var names by remember {
                     mutableStateOf(listOf<String>())
                 }
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
 
-                    Row (modifier = Modifier.fillMaxSize()){
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
 
-                        OutlinedTextField(value = name, onValueChange ={ text->
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
                             name = text
-                        } )
+                        },modifier =Modifier.weight(1f)
 
-                     Button(onClick = {
+                        )
+                     Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
 
-                         if(name.isNotBlank()){
+                            if (name.isNotBlank()) {
 
-                             Log.e("Button Clicked",name)
-                            names = names + name
-                         }
+                                Log.e("Button Clicked", name)
+                                names = names + name
+                                name = ""
+                            }
 
 
-                     }) {
-                         Text(text = "Add")
-                     }
+                        }) {
+                            Text(text = "Add")
+                        }
                     }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = PaddingValues(16.dp)
+                    ) {
 
-                    LazyColumn(){
-
-                        items(names){currentName -> Text(text = currentName)
+                        items(names) { currentName ->
+                            Text(text = currentName,
+                            modifier = Modifier.fillMaxWidth().padding(16.dp))
+                            Divider()
                         }
                     }
 
 
                 }
 
-                }
             }
         }
     }
+}
 
